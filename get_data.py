6516d3fc387be7d34b8db5ab12b4be93ccc0d1b4
aCampello <a.campello@wellcome.ac.uk>
2021-02-21 11:46:55 +0000

    Add index.html frontend

---
+++ get_data.py
@@ -0,0 +1,13 @@
+import requests
+r = requests.get('https://www.gov.uk/government/publications/register-of-licensed-sponsors-workers')
+
+from bs4 import BeautifulSoup
+
+html = BeautifulSoup(r.content)
+
+doc_links = [x for x in html.findAll('a') if 'tier_2_5' in x.attrs.get('href', '').lower()]
+
+r = requests.get(doc_links[-1].attrs.get('href'))
+
+with open('data.pdf', 'wb') as f:
+    f.write(r.content)
